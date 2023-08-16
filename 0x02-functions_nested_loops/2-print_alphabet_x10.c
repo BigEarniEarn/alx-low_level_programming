dce815e06ec9d26c3e69856c6b111f5a5932aba7
Earnest Ka Makhaya <bongilekb@gmail.com>
2023-08-16 09:50:34 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - A function that prints the alphabet x10 
+ * print_alphabet_x10 - A function that prints the alphabet x10
  * Return: void
  */
 
@@ -8,8 +9,8 @@ void print_alphabet_x10(void)
 {
 	char al;
 	int n;
-	n = 0;
 
+	n = 0;
 	while (n < 10)
 	{
 		for (al = 'a' ; al <= 'z' ; al++)
